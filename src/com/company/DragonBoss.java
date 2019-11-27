6006e433f63c37572e4bfced98099fb7c9b6ab11
Tonny <Tonny.Abrahamsson@hyhs.se>
2019-11-27 15:03:45 +0100

    Added Creature and subclasses

---
+++ src/com/company/DragonBoss.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class DragonBoss extends Monster{
+
+    public DragonBoss(String name, int health, int damage) {
+        super(name, health, damage);
+    }
+}
---
+++ src/com/company/Spider.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Spider extends Monster {
+
+    public Spider(String name, int health, int damage) {
+        super(name, health, damage);
+    }
+}

023778ca9199054c2cb749909351cf743f8fccb7
Sebbe Lundgren <moment928@gmail.com>
2019-11-29 13:45:18 +0100

    Added name and weight
    toString() and constuctor

--- src/com/company/Items.java
+++ src/com/company/Items.java
@@ -1,5 +1,27 @@
 package com.company;
 
 public class Items {
+    private String name;
+    private int weight;
 
+    public Items(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Items{" +
+                "name='" + name + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getName() {
+        return name;
+    }
 }

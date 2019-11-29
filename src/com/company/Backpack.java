4eca8cc660e653e7138b16aa3f478f30bcb72752
Sebbe Lundgren <moment928@gmail.com>
2019-11-29 14:23:09 +0100

    Added currentBagWeight()

--- src/com/company/Backpack.java
+++ src/com/company/Backpack.java
@@ -5,8 +5,16 @@ import java.util.ArrayList;
 public class Backpack {
     ArrayList<Items> itemsInBackpack = new ArrayList<>();
 
-    public addItem(Items items){
+    public void addItem(Items items){
         itemsInBackpack.add(items);
         System.out.println(itemsInBackpack);
+    }
+
+    public int currentBagWeight(){
+        int bagWeight = 0;
+        for (Items items : itemsInBackpack){
+            bagWeight += items.getWeight();
+        }
+        return bagWeight;
     }
 }

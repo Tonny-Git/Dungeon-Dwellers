b874c2a0973d9116b7777932dcd070697e2e624e
Sebbe Lundgren <moment928@gmail.com>
2019-11-29 14:06:19 +0100

    addItems() in bagpack and added classes
    CupOfCoffee and Toothbrush

--- src/com/company/Backpack.java
+++ src/com/company/Backpack.java
@@ -1,4 +1,12 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Backpack {
+    ArrayList<Items> itemsInBackpack = new ArrayList<>();
+
+    public addItem(Items items){
+        itemsInBackpack.add(items);
+        System.out.println(itemsInBackpack);
+    }
 }

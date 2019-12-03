0cfad6f882ba62313193ef9df8abca52f265bdfb
Tonny <Tonny.Abrahamsson@hyhs.se>
2019-12-03 19:37:47 +0100

    Added randomPosition Method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,26 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
         Maze maze = new Maze();
-        System.out.println("yo!");
+        maze.randomPosition();
+        /*
+        for(int i = 0; i < maze.mazePositions.length; i++) {
+            System.out.print(maze.mazePositions[i]);
+            if(i % maze.mapSize == maze.mapSize-1) {
+                System.out.println("");
+            }
+        }
+        */
+        //Maze maze = new Maze();
         //DungeonGame game = new DungeonGame();
 
-        System.out.println(maze.toString());
+        //System.out.println(maze.toString());
 
-
+        // 0 wall, 1 empty room, 2 monster, 3 item, 4 item/monster, 5 boss
     }
 }
--- src/com/company/Maze.java
+++ src/com/company/Maze.java
@@ -1,14 +1,16 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Maze{
 
-    private int mapSize = 15;
+    private int mapSize = 10;
     private Room[][] mazeArray;
-    
+    private int[] mazePositions = new int[mapSize*mapSize];
 
+    /*
     public Maze() {
 
         Scanner scanner = new Scanner(System.in);
@@ -36,6 +38,8 @@ public class Maze{
         System.out.println(mazeArray);
     }
 
+
+     */
     public Room getMazeRoom(int xPosition, int yPosition) {
 
         return mazeArray[xPosition][yPosition];
@@ -75,6 +79,48 @@ public class Maze{
         return out;
     }
 
+    public void randomPosition () {
+        int numOfMonster = (int)Math.floor((Math.random()*5)+3);
+        int numOfItems = (int)Math.floor((Math.random()*3)+2);
+        int numOfMonsterAndItems = (int)Math.floor((Math.random()*4));
+        ArrayList<Integer> randomPositions = randomMonsterAndItemPosition(numOfMonster, numOfItems, numOfMonsterAndItems);
+
+        for(int i = 0; i < mapSize*mapSize; i++) {
+            if(i % mapSize == 0 || i % mapSize == mapSize-1 || i < mapSize || i > mapSize*mapSize - mapSize) {
+                mazePositions[i] = 0;
+            } else if(randomPositions.contains(i)) {
+                if(randomPositions.indexOf(i) < numOfMonster) {
+                    mazePositions[i] = 2;
+                } else if(randomPositions.indexOf(i) < numOfMonster+numOfItems && randomPositions.indexOf(i) >= numOfMonster) {
+                    mazePositions[i] = 3;
+                } else if (randomPositions.indexOf(i) < numOfMonster+numOfItems+numOfMonsterAndItems && randomPositions.indexOf(i) >= numOfMonster+numOfItems) {
+                    mazePositions[i] = 4;
+                } else {
+                    mazePositions[i] = 5;
+                }
+            } else {
+                mazePositions[i] = 1;
+            }
+        }
+    }
+
+    private ArrayList<Integer> randomMonsterAndItemPosition (int numOfMonster, int numOfItems, int numOfMonsterAndItems) {
+        ArrayList<Integer> randomPositions = new ArrayList<>();
+        int numTotalt = numOfMonster + numOfItems + numOfMonsterAndItems + 1; //1 For the dragon boss
+        for(int i = 0; i < numTotalt; i++) {
+            int randomNum = (int)Math.floor(Math.random()*(mapSize*mapSize));
+            if(randomNum % mapSize == 0 || randomNum % mapSize == mapSize-1 || randomNum < mapSize || randomNum > mapSize*mapSize - mapSize) {
+                i--;
+            } else {
+                if(!randomPositions.contains(randomNum)) {
+                    randomPositions.add(randomNum);
+                } else {
+                    i--;
+                }
+            }
+        }
+        return randomPositions;
+    }
 
     @Override
     public String toString() {

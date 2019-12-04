93e6ea4b85e6f6bbc2cabdc2edb5abad6e06e153
Mir <hanneswartel@gmail.com>
2019-12-04 14:15:34 +0100

    Randomizers for room population items och gold tillagda.

--- src/com/company/Room.java
+++ src/com/company/Room.java
@@ -6,8 +6,9 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Room extends Position {
 
 
-    CupOfCoffee coffe = new CupOfCoffee("Covfefe", 10);
-    Toothbrush toothbrush = new Toothbrush("Tooth brush", 1);
+    CupOfCoffee coffe = new CupOfCoffee("Covfefe", 20);
+    Toothbrush toothbrush = new Toothbrush("Tooth brush", 3);
+    Gold gold = new Gold("Gold Coin", 1);
     Bandit bandit = new Bandit();
     Spider spider = new Spider();
     DragonBoss dragon = new DragonBoss();
@@ -22,12 +23,13 @@ public class Room extends Position {
 
 
     private boolean hasWall = false;
-    private int randomNum = ThreadLocalRandom.current().nextInt(0, 3 + 1);
+
 
     public Room(int posX, int poxY, int roomType) {
         super(posX, poxY);
         this.roomType = roomType;
-        makeRoom(roomType);
+
+        makeRoom(this.roomType);
 
 
     }
@@ -38,17 +40,18 @@ public class Room extends Position {
                 hasWall = true;
                 break;
             case 1:
-                ;
+
                 break;
             case 2:
-                populateRoom(randomNum);
+                placeLootRoom();
+;
                 break;
             case 3:
-                placeLootRoom(randomNum);
+                populateRoom();
                 break;
             case 4:
-                placeLootRoom(randomNum);
-                populateRoom(randomNum);
+                placeLootRoom();
+                populateRoom();
                 break;
 
 
@@ -61,37 +64,51 @@ public class Room extends Position {
     }
 
 
-    public void populateRoom(int roomNumber) {
+    public void populateRoom() {
+        int randomNum = ThreadLocalRandom.current().nextInt(0, 1 + 1);
 
-        switch (roomNumber) {
-            case 1:
+        switch (randomNum) {
+            case 0:
                 roomCreatures.add(bandit);
                 break;
-            case 2:
-                roomCreatures.add(spider);
-                break;
-            case 3: // item
-                break;
-            case 4: // boss
-
-        }
-    }
-
-    private void placeLootRoom(int roomNumber) {
-
-        switch (roomNumber) {
             case 1:
-                roomItems.add(coffe);
-                break;
-            case 2:
-                roomItems.add(toothbrush);
-                break;
-            case 3: // item
-                break;
-            case 4: // boss
+                roomCreatures.add(spider);
+
 
         }
     }
+
+    private void placeLootRoom() {
+        int randomNum = ThreadLocalRandom.current().nextInt(0, 2 + 1);
+
+        switch (randomNum) {
+            case 0:
+                roomItems.add(coffe);
+                placeGoldRoom();
+                break;
+            case 1:
+                roomItems.add(toothbrush);
+                placeGoldRoom();
+                break;
+
+            case 2:
+                roomItems.add(gold);
+                placeGoldRoom();
+
+        }
+    }
+
+
+    private void placeGoldRoom() {
+        int randomNum = ThreadLocalRandom.current().nextInt(0, 1 + 1);
+
+        while (randomNum == 0) {
+            roomItems.add(gold);
+            randomNum = ThreadLocalRandom.current().nextInt(0, 1 + 1);
+
+        }
+    }
+
 
     public ArrayList<Creature> getRoomCreatures() {
         return roomCreatures;
@@ -99,6 +116,10 @@ public class Room extends Position {
 
     public ArrayList<Items> getRoomItems() {
         return roomItems;
+    }
+
+    public String getRoomInfo() {
+        return getWall() + " " + getRoomCreatures() + " " + getRoomItems();
     }
 
     @Override

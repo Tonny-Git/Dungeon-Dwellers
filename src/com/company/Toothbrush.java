9b6f18f242c2b6996d389b0835b5726089291d91
Sebbe Lundgren <moment928@gmail.com>
2019-12-10 10:42:26 +0100

    Can and cant do quest

--- src/com/company/Toothbrush.java
+++ src/com/company/Toothbrush.java
@@ -1,8 +1,17 @@
 package com.company;
 
 public class Toothbrush extends Items {
+    private int numOfToothbrush;
 
     public Toothbrush(String name, int weight) {
         super(name, weight);
+    }
+
+    public void usedToothbrush{
+        if (numOfToothbrush == 0){
+            System.out.println("You cant do quest");
+        } else {
+            System.out.println("You can do the quest");
+        }
     }
 }

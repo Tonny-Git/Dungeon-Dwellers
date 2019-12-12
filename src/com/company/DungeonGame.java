799d004afe58ce6961ab86191b25a766cba5b102
Tonny <Tonny.Abrahamsson@hyhs.se>
2019-12-12 16:08:56 +0100

    fixed boss input bug

--- src/com/company/DungeonGame.java
+++ src/com/company/DungeonGame.java
@@ -176,7 +176,7 @@ public class DungeonGame implements Serializable {
 
     private void boss() {
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+        String input;
 
         System.out.println(hero.getName() + " encountered The Elder Dragon!");
         if (hero.getTotalExp() >= 500 && hero.hasToothbrush()) {
@@ -190,6 +190,7 @@ public class DungeonGame implements Serializable {
                             "She wants you to find her toothbrush that's been lost in this dungeon." + "\n" +
                             "IF you retrieve it for her she will reward you greatly." + "\n" +
                             "Do you accept? Y/N");
+            input = scanner.nextLine();
             if (input.toLowerCase().equals("y")) {
                 if (keydropped < 1) {
                     //dropToothbrushSomewhereInDungeon();
